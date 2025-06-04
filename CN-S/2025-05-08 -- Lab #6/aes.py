74c5260f6cffb83429768630e6356db033ce9fcf
THeZoNE-007 <ashutoshmallkvctc@gmail.com>
2025-06-05 02:27:18 +0530

    cn

--- CN-S/2025-05-08 -- Lab #6/aes.py
+++ CN-S/2025-05-08 -- Lab #6/aes.py
@@ -4,26 +4,26 @@ import binascii
 def encryption(aes, P, key):
     while (len(P) % 16 != 0):
         P += " "
-    P = P.encode()            
-    C = aes.encrypt(P)       
-    C = binascii.hexlify(C)   
-    C = C.decode()             
+    P = P.encode()              # Converts string to binary
+    C = aes.encrypt(P)          # Calls the encrypt function
+    C = binascii.hexlify(C)     # Return the hexadecimal representation of the binary C.
+    C = C.decode()              # Converts binary C to string
     return C
 
 def decryption(aes, C, key):
-    C = binascii.unhexlify(C)   
-    P1 = aes.decrypt(C)        
-    P1 = P1.decode().strip()    
+    C = binascii.unhexlify(C)   # Return the binary data represented by the hexadecimal string hexstr.
+    P1 = aes.decrypt(C)         # Calls decrypt function
+    P1 = P1.decode().strip()    # Converts binary C to string and remove padding spaces.
     return P1;
 
 key = input("Enter key: ")
 P = input("Enter plaintext: ")
 print("Plaintext is: ",P)
-key = key.encode()            
-aes_cipher = AES.new(key, AES.MODE_ECB) 
+key = key.encode()              # key is converted to binary. AES.new() takes the key in binary
+aes_cipher = AES.new(key, AES.MODE_ECB) # Creates a AES cipher object.
 
-C = encryption(aes_cipher, P, key)      
+C = encryption(aes_cipher, P, key)      # Calls encryption function with the ciper object, plaintext and key.
 print("Ciphertext after encryption: ",C)
 
-P1 = decryption(aes_cipher, C, key)     
+P1 = decryption(aes_cipher, C, key)     # # Calls decryption function with the ciper object, ciphertext and key.
 print("Plaintext after decryption: ",P1)
